93af85a4eb07feb04175a279af84faa32d8d7e68
aluno <etecae023@hotmail.com>
2025-09-12 14:56:29 -0300

    Botão de Voltar de Config funcionando

--- src/app/config/config.page.ts
+++ src/app/config/config.page.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-config',
@@ -17,6 +18,10 @@ import { FormsModule } from '@angular/forms';
 
   export class ConfigPage {
 
-  constructor() {}
+  constructor(private navCtrl: NavController) {}
+
+  voltar() {
+    this.navCtrl.back();
+  }
 
 }
