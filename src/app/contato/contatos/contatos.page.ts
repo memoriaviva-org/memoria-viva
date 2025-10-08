c4b4185c8e4534f66bf3512d01e0d7d2c3a6c052
aluno <etecae023@hotmail.com>
2025-10-08 17:41:56 -0300

    boneca do contatos

--- src/app/contato/contatos/contatos.page.ts
+++ src/app/contato/contatos/contatos.page.ts
@@ -37,18 +37,18 @@ export class ContatosPage implements OnInit {
     const button = document.querySelector('.audio-btn') as HTMLElement;
 
     if (audio.paused) {
-        // Esconde botão e mostra player
-        button.style.display = 'none';
-        audio.style.display = 'block';
-        audio.play();
-      } else {
-        audio.pause();
-      }
-
-        // Quando terminar, esconde player e volta botão
-        audio.onended = () => {
-        audio.style.display = 'none';
-        button.style.display = 'inline-flex'; // volta o ion-button
-      };
+      button.style.display = 'none';
+      audio.style.display = 'block';
+      audio.play().catch(err => console.error('Erro ao reproduzir áudio:', err));
+    } else {
+      audio.pause();
     }
+
+    // Quando o áudio termina, volta ao botão
+    audio.onended = () => {
+      audio.style.display = 'none';
+      button.style.display = 'inline-flex';
+    };
+  }
+
 }
